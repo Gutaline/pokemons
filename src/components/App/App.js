9cb6148437d464b48656361e685f278f92ac6611
Asiimov <1plus1rovno1@gmail.com>
2021-08-07 11:10:27 +0300

    style fix  and downloads poki fix

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,80 +1,38 @@
+import { useEffect, useState } from 'react';
 import Container from '../Container/Container';
 
 function App() {
-  let a = [
-    {
-      id: 1,
-      name: 'bulba',
-      sprites: {
-        other: {
-          dream_world: {
-            front_default:
-              'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/2.svg',
-          },
-        },
-      },
-      types: [{ type: 'grass' }, { type: 'grass' }],
-    },
-    {
-      id: 2,
-      name: 'bulba',
-      sprites: {
-        other: {
-          dream_world: {
-            front_default:
-              'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/2.svg',
-          },
-        },
-      },
-      types: [{ type: 'grass' }, { type: 'grass' }],
-    },
-    {
-      id: 3,
-      name: 'bulba',
-      sprites: {
-        other: {
-          dream_world: {
-            front_default:
-              'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/2.svg',
-          },
-        },
-      },
-      types: [{ type: 'grass' }, { type: 'grass' }],
-    },
-  ];
+  const [allPokemons, setAllPokemons] = useState([]);
+  const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=20');
+
+  const getAllPokemons = async () => {
+    const res = await fetch(loadMore);
+    const data = await res.json();
+
+    setLoadMore(data.next);
+
+    function createPokemonObject(results) {
+      results.forEach(async pokemon => {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`);
+        const data = await res.json();
+        setAllPokemons(currentList => [...currentList, data]);
+        await allPokemons.sort((a, b) => a.id - b.id);
+      });
+    }
+
+    createPokemonObject(data.results);
+  };
+
+  useEffect(() => {
+    getAllPokemons();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div className="App">
-      <Container pokemonsList={a} />
+      <Container pokemonsList={allPokemons} />
     </div>
   );
 }
 
 export default App;
-
-// import { useEffect, useState } from 'react';
-
-// const [allPokemons, setAllPokemons] = useState([]);
-// const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=20');
-
-// const getAllPokemons = async () => {
-//   const res = await fetch(loadMore);
-//   const data = await res.json();
-
-//   setLoadMore(data.next);
-
-//   function createPokemonObject(results) {
-//     results.forEach(async pokemon => {
-//       const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`);
-//       const data = await res.json();
-//       setAllPokemons(currentList => [...currentList, data]);
-//       await allPokemons.sort((a, b) => a.id - b.id);
-//     });
-//   }
-
-//   createPokemonObject(data.results);
-// };
-
-// useEffect(() => {
-//   getAllPokemons();
-// }, []);
