6eb3b589a341dc2c4ee53d1e2db630143318d4f9
Asiimov <1plus1rovno1@gmail.com>
2021-08-10 18:09:11 +0300

    Sort poki api

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,65 +1,62 @@
-import { useEffect, useState } from 'react'
-import Container from '../Container/Container'
+import { useEffect, useState } from 'react';
+import Container from '../Container/Container';
 
 function App() {
-  const [allPokemons, setAllPokemons] = useState([])
-  const [loadMore, setLoadMore] = useState(
-    'https://pokeapi.co/api/v2/pokemon?limit=20'
-  )
+  const [allPokemons, setAllPokemons] = useState([]);
+  const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=20');
 
   const getAllPokemons = async () => {
-    const res = await fetch(loadMore)
-    const data = await res.json()
+    const res = await fetch(loadMore);
+    const data = await res.json();
 
-    setLoadMore(data.next)
+    setLoadMore(data.next);
 
     function createPokemonObject(results) {
-      results.forEach(async (pokemon) => {
-        const res = await fetch(
-          `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
-        )
-        const data = await res.json()
+      results.forEach(async pokemon => {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`);
+        const data = await res.json();
 
-        setAllPokemons((currentList) => [...currentList, data])
-        await allPokemons.sort((a, b) => a.id - b.id)
-      })
+        setAllPokemons(currentList => [...currentList, data]);
+      });
     }
 
-    createPokemonObject(data.results)
-  }
+    createPokemonObject(data.results);
+  };
+
+  let sortPoki = allPokemons;
+  sortPoki.sort((a, b) => a.id - b.id);
 
   useEffect(() => {
-    getAllPokemons()
+    getAllPokemons();
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  }, []);
 
-  console.log(allPokemons);
   function checkFavorPoki() {
-    const a = JSON.parse(localStorage.getItem('poki'))
-    let b = []
+    const a = JSON.parse(localStorage.getItem('poki'));
+    let b = [];
 
     if (a) {
       a.forEach((item, index) => {
-        b[index] = item.id
-      })
+        b[index] = item.id;
+      });
 
-      allPokemons.forEach((item) => {
+      allPokemons.forEach(item => {
         if (b.includes(item.id)) {
-          item.favor = true
+          item.favor = true;
         }
-      })
+      });
     }
   }
 
-  checkFavorPoki()
+  checkFavorPoki();
   // console.log(allPokemons);
 
   return (
     <div className="App">
-      <Container pokemonsList={allPokemons} />
+      <Container pokemonsList={sortPoki} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
