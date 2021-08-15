1b506e843ee8efcb40a52b6d35afdc6b7c81e67c
Eugene <evgeniq.makcimov@yandex.ru>
2021-08-15 21:23:37 +0300

    test view

--- src/components/Container/PokemonDescription.jsx
+++ src/components/Container/PokemonDescription.jsx
@@ -1,46 +1,67 @@
-
 import React from 'react'
 import heart from '../../assets/Heart1.png'
 
-function PokemonDescription({id,name,image,type,favorites,showHeartDescr,setShowHeartDescr,allDescr}) {
-
-    
-  
+function PokemonDescription({
+  id,
+  name,
+  image,
+  type,
+  favorites,
+  showHeartDescr,
+  setShowHeartDescr,
+  allDescr,
+  effect,
+}) {
   function addOrDeleteFavor(params) {
-    if(params) {
-      favorites((currentList)=> [...currentList , {id: id ,name: name, image: image, type:type , favor: true}]);
+    if (params) {
+      favorites((currentList) => [
+        ...currentList,
+        { id: id, name: name, image: image, type: type, favor: true },
+      ])
       setShowHeartDescr(!params)
     } else {
-      const todos = JSON.parse(localStorage.getItem('poki'));
-      
-      todos.forEach((item,index) => {
-        if(item.id === id )  {
-          todos.splice(index,1);
+      const todos = JSON.parse(localStorage.getItem('poki'))
+
+      todos.forEach((item, index) => {
+        if (item.id === id) {
+          todos.splice(index, 1)
         }
       })
-      favorites(todos);
-      setShowHeartDescr(!params);
-      allDescr.favor = false;
-    } 
+      favorites(todos)
+      setShowHeartDescr(!params)
+      allDescr.favor = false
+    }
   }
- 
-  
-    return (
-      <div>
-       <div className = "view-description">
-        <div className = "view-description__name">{name}</div> 
-        <div className = "view-description__image"><img src={image} alt="" /></div>
-        <div className = "view-description__type">Class:{type}</div>
-        <div className = "view-description__heart" >
-            
-          <div onClick = {() => addOrDeleteFavor(showHeartDescr) }>
-            {<img className ={allDescr.favor ? "view-description__heart--active":"view-description__heart--noactive" } src={heart} alt="heart" onClick = {() => { allDescr.favor = true} }  />} 
+
+  return (
+    <div>
+      <div className="view-description">
+        <div className="view-description__name">{name}</div>
+        <div className="view-description__image">
+          <img src={image} alt="" />
+        </div>
+        <div className="view-description__type">Class:{type}</div>
+        <div className="view-description__heart">
+          <div onClick={() => addOrDeleteFavor(showHeartDescr)}>
+            {
+              <img
+                className={
+                  allDescr.favor
+                    ? 'view-description__heart--active'
+                    : 'view-description__heart--noactive'
+                }
+                src={heart}
+                alt="heart"
+                onClick={() => {
+                  allDescr.favor = true
+                }}
+              />
+            }
           </div>
-      
-          </div>
-       </div>
+        </div>
       </div>
-    )
+    </div>
+  )
 }
 
 export default PokemonDescription
--- src/components/Container/View.jsx
+++ src/components/Container/View.jsx
@@ -1,24 +1,28 @@
 import React from 'react'
 import PokemonDescription from './PokemonDescription'
 
-function View({myState,pokiDescription,favorites,showHeartDescr,setShowHeartDescr}) {
-
- 
+function View({
+  myState,
+  pokiDescription,
+  favorites,
+  showHeartDescr,
+  setShowHeartDescr,
+}) {
   return (
-    <div  className = "view" >
-     { myState &&
-        <PokemonDescription  
-        favorites = {favorites}  
-        allDescr = {pokiDescription}
-        id = {pokiDescription.id}
-        name = {pokiDescription.name}
-        image = {pokiDescription.sprites.other.dream_world.front_default}
-        type = {pokiDescription.types[0].type.name}
-        heartVisible = {pokiDescription.favor}
-        showHeartDescr = {showHeartDescr}
-        setShowHeartDescr = {setShowHeartDescr}
+    <div className="view">
+      {myState && (
+        <PokemonDescription
+          favorites={favorites}
+          allDescr={pokiDescription}
+          id={pokiDescription.id}
+          name={pokiDescription.name}
+          image={pokiDescription.sprites.other.dream_world.front_default}
+          type={pokiDescription.types[0].type.name}
+          heartVisible={pokiDescription.favor}
+          showHeartDescr={showHeartDescr}
+          setShowHeartDescr={setShowHeartDescr}
         />
-     } 
+      )}
     </div>
   )
 }
