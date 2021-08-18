8b141db9005c413893390a2981db729fc495267b
Eugene <evgeniq.makcimov@yandex.ru>
2021-08-18 12:02:53 +0300

    press the icon and by happy without scroll

--- src/components/Container/Header.tsx
+++ src/components/Container/Header.tsx
@@ -5,26 +5,25 @@ import ball from '../../assets/ball-1.png'
 export const Header: React.FC<{
   burger: boolean
   setBurger: (burger: boolean) => void
-}> = ({burger,setBurger}) => {
- 
-
+}> = ({ burger, setBurger }) => {
   return (
-    
     <div className="header">
       <div className="header-logo_ball">
         <img src={ball} alt="ball" />
       </div>
       <div className="header-logo__pokeman">
-        <img src={logo} alt="pokemon" />
+        <img src={logo} alt="pokemon"onClick={() => window.location.reload()} />
       </div>
-      
-        <div className = "burger-menu" onClick = {() =>{setBurger(!burger)}}>
-          <span></span>
-        </div>
-      
-  
+
+      <div
+        className="burger-menu"
+        onClick={() => {
+          setBurger(!burger)
+        }}
+      >
+        <span></span>
+      </div>
     </div>
-    
   )
 }
 
